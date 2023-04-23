61ece55da07616a2f791effaf08f111b08f97f97
ronlemire2 <ronlemire2@gmail.com>
2023-04-23 11:09:37 -0700

    Initial Commit

---
+++ Quizes/QuizesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizesApp.swift
+//  Quizes
+//
+//  Created by Ron Lemire on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
